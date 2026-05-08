bddd7881e4e79e55719fb0ff71a406dfe10e7c4b
Ashish Shukla <ashish.shukla@nagarro.com>
2026-05-08 15:39:29 +0530

    Added custoner Controller

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PRReviewDemo.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+
+		/// <summary>
+		/// id property
+		/// </summary>
+		public string name { get; set; }
+    }
+}
